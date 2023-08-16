fa6ef2527b646912c0a3c835c20f9bec51edc410
Dmitry Dmitry <7353687@gmail.com>
2023-08-15 20:33:28 -0400

    fixed bugs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 let notes = require('./db/db');
 const uuid = require('./helpers/uuid');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
